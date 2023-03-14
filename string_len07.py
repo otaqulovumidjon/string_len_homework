10285eb81cd9063cd4213fd9634542edf5971e30
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-03-14 07:59:47 +0500

    len07

--- string_len07.py
+++ string_len07.py
@@ -8,21 +8,38 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    if len(s1)%2==1 and len(s1)>0:
+    if len(s1)%2==1:
         x1 = s1
     else:
         x1 = ""
 
-    if len(s2)%2==1 and len(s2)>0:
-        x2 = ', '+s2
+
+    if len(s2)%2==1:
+        if len(s1)%2!=0:
+            x2 = ', '+s2
+        else:
+            x2 = s2
     else:
         x2 = ""
 
-    if len(s3)%2==1 and len(s3)>0:
-        x3 = ', '+s3
+
+    if len(s3)%2==1:
+        if len(s1)%2!=0:
+            x3 = ', '+s3
+        else:
+            x3 = s3
+    else:
+        x3 = ""
+
+
+    if len(s3)%2==1:  
+        if len(s2)%2!=0:
+            x3 = ', '+s3
+        else:
+            x3 = ', '+s3
     else:
         x3 = ""
 
     return f"[{x1}{x2}{x3}]"
 
-print(main(s1="codeschool.uz", s2="example", s3="python"))
+print(main("12345", "1234567", "123456789"))
